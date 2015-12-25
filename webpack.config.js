e110f7ffc0f2fbaba3dfa941cf4ade2a3eee554c
Saemie <saemie@chouchane.com>
2015-12-25 11:15:25 +0000

    Configure webpack development server; Enable Hot Module Repacement (HMR) and enable HTML5 History API fallback.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -12,7 +13,21 @@ module.exports = {
     path: PATHS.build,
     filename: 'bundle.js'
   },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+
+    // Display only errors to limit the amount of output.
+    stats: 'errors-only',
+
+    // Parse host and port from env so this is easy to customise.
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       title: 'Kanban app'
     })
