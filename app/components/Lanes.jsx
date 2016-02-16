4189b7d16a9abbea4604401ed3ca5a4b3c3cbb0f
Saemie <saemie@chouchane.com>
2016-02-16 18:49:25 +0000

    Simple Lanes component for now

---
+++ app/components/Lanes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Lanes extends React.Component {
+  render() {
+    return (
+        <div className="lanes">
+          Hi there, this is where the lanes will go.
+        </div>
+    )
+  }
+}
